26ec7ead7794ca1c8a35be749a429d2a3f555a6a
Ema Lorenzo <aemalorenzo@gmail.com>
2021-05-16 19:13:07 -0300

    tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,23 @@ module.exports = {
   // purge: ['./src/pages/**/*.jsx', './src/components/**/*.jsx'], // remove unused styles in production
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      display: 'Poppins',
+      sans: 'Poppins',
+    },
+    extend: {
+      colors: {
+        gray: {
+          900: '#141414',
+        },
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      borderWidth: ['hover', 'focus'],
+      ringWidth: ['hover', 'active'],
+    },
   },
   plugins: [],
 }
