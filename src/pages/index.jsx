84adef37092cc1336ebd46700efdc8523e8b5a9d
Ema Lorenzo <aemalorenzo@gmail.com>
2021-05-16 18:47:05 -0300

    show home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,37 @@
-import useStore from '@/helpers/store'
 import dynamic from 'next/dynamic'
-// Step 5 - delete Instructions components
 
-// Step 2 - update Box components
 const Box = dynamic(() => import('@/components/canvas/Box'), {
   ssr: false,
 })
 
-const Page = ({ title }) => {
-  useStore.setState({ title })
+const Home = ({ modules }) => {
   return (
     <>
+      <div className='min-h-screen'>
+        {/* hero */}
+        <section className='flex items-center h-screen p-20 pt-0'>
+          <div className='flex flex-col max-w-4xl gap-y-4'>
+            <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 font-display'>
+              Compartimos conocimiento,
+              <br /> Aprendemos juntos
+            </h1>
+            <h3 className='text-gray-600'>
+              Somos una comunidad de personas en tech que aprende y comparte
+              conocimiento. No sabemos todo, el mundo tech es enorme y lleva una
+              vida de aprendizaje, pero mientras vamos aprendiendo creemos que
+              compartido el viaje es mejor. Por eso incentivamos siempre a que
+              una vez se aprenda algo, le que quiera pueda compartirlo y asi
+              crecer juntos.
+              <br />
+              Coderhood es y siempre va a ser gratis, tanto la comunidad, como
+              las clases y cualquiera de sus espacios.
+            </h3>
+          </div>
+        </section>
+      </div>
       <Box r3f route='/box' />
-      {/* Step 5 - delete Instructions components */}
     </>
   )
 }
 
-export default Page
-
-export async function getStaticProps() {
-  return {
-    props: {
-      title: 'Index',
-    },
-  }
-}
+export default Home
