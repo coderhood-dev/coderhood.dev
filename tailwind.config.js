79b15880b491242af590c8062607c5b3ab4eb0bd
Ema Lorenzo <aemalorenzo@gmail.com>
2021-05-19 18:34:31 -0300

    purge files again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   // mode: 'jit',
-  // purge: [
-  //   './src/pages/**/*.{js,ts,jsx,tsx}',
-  //   './src/components/**/*.{js,ts,jsx,tsx}',
-  // ],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     screens: {
