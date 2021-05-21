3567dd21a40d8fe98aa9cccac36e38650439af01
Ema Lorenzo <aemalorenzo@gmail.com>
2021-05-21 04:03:28 -0300

    using real pictures

--- src/data/learningBubbles.js
+++ src/data/learningBubbles.js
@@ -1,25 +1,25 @@
 export const learningBubbles = [
   {
     status: 3,
-    img: '/img/kakashi.jpeg',
+    img: '/img/kakashi.png',
     position: { right: '8rem', top: '0.5rem' },
     animate: { y: [20, 0, -5, 10, 0], x: [10, 0, -7, 1, 0] },
   },
   {
     status: 1,
-    img: '/img/sasuke.jpeg',
+    img: '/img/meison.jpeg',
     position: { right: '3rem', top: '10rem' },
     animate: { y: [10, 0, 5, -10, 0], x: [5, 0, 7, -5, 0] },
   },
   {
-    status: 1,
-    img: '/img/naruto.png',
+    status: 0,
+    img: '/img/lucas.jpg',
     position: { right: '15rem', top: '9rem' },
     animate: { y: [7, 0, -3, 5, 0], x: [-5, 0, -10, 5, 0] },
   },
   {
-    status: 0,
-    img: '/img/konohamaru.png',
+    status: -1,
+    img: '/img/bab10.jpeg',
     position: { right: '12rem', top: '20rem' },
     animate: { y: [7, 0, -3, 5, 0], x: [-5, 0, -10, 5, 0] },
   },
