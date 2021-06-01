3a8e6a0a02fab8930d0e0f85040f267fd379b344
Ema Lorenzo <aemalorenzo@gmail.com>
2021-06-01 01:42:14 -0300

    pdf terminado?

--- src/lib/files.js
+++ src/lib/files.js
@@ -21,17 +21,8 @@ export async function getLesson(lessonURL, moduleURL) {
   const moduleName = await getCompleteName(moduleURL, 'academy')
   const lessonName = await getCompleteName(lessonURL, `academy/${moduleName}`)
 
-  const lessonPath = `academy/${moduleName}/${lessonName}`
-  const lessonContent = await getFolderContent(lessonPath)
-
   const mdx = await getMDX(`academy/${moduleName}/${lessonName}/readme.mdx`)
-
-  const pdfAvailable = lessonContent.some((file) => file.includes('.pdf'))
-  let pdfURL = null
-
-  if (pdfAvailable) {
-    pdfURL = await getPDF(moduleName, lessonName)
-  }
+  const pdfURL = await getPDF(moduleName, lessonName)
 
   return { ...mdx, pdfURL }
 }
@@ -50,8 +41,6 @@ export async function getLessons(moduleURL) {
     const lessonPath = `academy/${moduleName}/${lesson}`
     const lessonContent = await getFolderContent(lessonPath)
 
-    // TODO: chequear si existe el archivo en github, no local
-    // este chequeo local hace que si se accede a un pdf que no esta en github en local rompa
     if (lessonContent.includes('readme.mdx')) {
       const l = buildLessonStrings(lesson, `/academy/${moduleURL}`)
       lessons.push(l)
@@ -59,18 +48,6 @@ export async function getLessons(moduleURL) {
   }
 
   return lessons // [{ url: string, title: string }]
-}
-
-export async function getPDF(moduleName, lessonName) {
-  const url = `${process.env.REPO_URL}/contents/public/data/academy/${moduleName}/${lessonName}?ref=master`
-
-  const lessonFiles = await fetch(url, {
-    headers: { Authorization: `token ${process.env.GITHUB_OAUTH_TOKEN}` },
-  }).then((r) => r.json())
-
-  const { download_url } = lessonFiles.find(({ name }) => name.includes('.pdf'))
-
-  return download_url
 }
 
 export async function getMDX(url) {
@@ -86,4 +63,21 @@ export async function getMDX(url) {
       ...data,
     },
   }
+}
+
+// Get PDF download link from GitHub repo
+export async function getPDF(moduleName, lessonName) {
+  const githubURL = `${process.env.REPO_URL}/contents/public/data/academy/${moduleName}/${lessonName}?ref=master`
+
+  const githubLessonFiles = await fetch(githubURL, {
+    headers: { Authorization: `token ${process.env.GITHUB_OAUTH_TOKEN}` },
+  }).then((r) => r.json())
+
+  if (githubLessonFiles) {
+    const pdfFile = githubLessonFiles.find(({ name }) => name.includes('.pdf'))
+
+    return pdfFile ? pdfFile.download_url : null
+  }
+
+  return null
 }
