4c6b5f6d8c9cd9cb9152aa9bfa78b5018f934873
Ema Lorenzo <aemalorenzo@gmail.com>
2021-06-04 18:30:43 -0300

    dark mode for typography plugin, used on mdx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,9 +21,51 @@ module.exports = {
     extend: {
       colors: {
         gray: {
+          800: '#34373A',
           900: '#141414',
         },
       },
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            a: {
+              color: theme('colors.blue.400'),
+              '&:hover': {
+                color: theme('colors.blue.600'),
+              },
+              code: { color: theme('colors.blue.400') },
+            },
+            blockquote: {
+              borderLeftColor: theme('colors.gray.700'),
+              color: theme('colors.gray.300'),
+            },
+            'h2,h3,h4': {
+              color: theme('colors.gray.100'),
+            },
+            hr: { borderColor: theme('colors.gray.700') },
+            ol: {
+              li: {
+                '&:before': { color: theme('colors.gray.500') },
+              },
+            },
+            ul: {
+              li: {
+                '&:before': { backgroundColor: theme('colors.gray.500') },
+              },
+            },
+            strong: { color: theme('colors.gray.300') },
+            thead: {
+              color: theme('colors.gray.100'),
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme('colors.gray.700'),
+              },
+            },
+          },
+        },
+      }),
     },
   },
   variants: {
@@ -31,9 +73,6 @@ module.exports = {
       borderWidth: ['hover', 'focus'],
       ringWidth: ['hover', 'active'],
       textColor: ['dark'],
-      // width: ['dark'],
-      // height: ['dark'],
-      // overflow: ['dark'],
     },
     typography: ['dark'],
   },
