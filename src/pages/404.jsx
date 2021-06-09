d914d9dfcf4825ae529fc9c26c70cfff9e57509a
Ema Lorenzo <aemalorenzo@gmail.com>
2021-06-09 03:09:40 -0300

    container on default 404 and 500

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,12 @@
+import { Container } from '@/components/Container'
+
 // custom pages/404.js !! Do not remove please or it will break build
 export default function Error() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <>
+      <Container title='error'>
+        <h1>404 - Page Not Found</h1>
+      </Container>
+    </>
+  )
 }
--- src/pages/500.jsx
+++ src/pages/500.jsx
@@ -1,4 +1,12 @@
+import { Container } from '@/components/Container'
+
 // custom pages/500.js !! Do not remove please or it will break build
 export default function Error() {
-  return <h1>500 - Something went wrong</h1>
+  return (
+    <>
+      <Container title='error'>
+        <h1>500 - Something went wrong</h1>
+      </Container>
+    </>
+  )
 }
