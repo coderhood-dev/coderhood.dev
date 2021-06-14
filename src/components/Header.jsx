efdcced6a943e40c19fee5b946b456a509b975b2
Ema Lorenzo <aemalorenzo@gmail.com>
2021-06-14 16:25:52 -0300

    beta badge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,17 +18,24 @@ export const Header = () => {
     <>
       <Head title={title} />
       <header className='flex items-center justify-between h-20 pl-10 pr-16 border-b dark:border-b-0'>
-        <Link href='/'>
-          <a className='flex w-32 sm:w-60'>
-            <Image
-              src='/images/logo-full.png'
-              alt='Coderhood logo'
-              height={42}
-              width={192}
-              quality={65}
-            />
-          </a>
-        </Link>
+        <div className='flex items-center justify-start'>
+          <Link href='/'>
+            <a className='flex w-30 sm:w-48'>
+              <Image
+                src='/images/logo-full.png'
+                alt='Coderhood logo'
+                height={42}
+                width={192}
+                quality={65}
+              />
+            </a>
+          </Link>
+          {process.env.NODE_ENV !== 'production' && (
+            <span className=' p-1 ml-2 text-xs text-yellow-200 bg-yellow-500 rounded'>
+              BETA
+            </span>
+          )}
+        </div>
         <nav className='flex pl-10'>
           <ul>
             <AnimatePresence>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     preserveHtmlElements: false,
     content: [
       './src/pages/**/*.{js,ts,jsx,tsx}',
-      './src/components/**/*.{js,ts,jsx,tsx}',
+      './src/components/*.{js,ts,jsx,tsx}',
       './src/layouts/**/*.{js,ts,jsx,tsx}',
     ],
   },
