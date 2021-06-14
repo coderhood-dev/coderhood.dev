c570b7b7ecfe12e328c6b984af26673f4e4bde34
Ema Lorenzo <aemalorenzo@gmail.com>
2021-06-14 16:36:01 -0300

    corrected env name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ export const Header = () => {
               />
             </a>
           </Link>
-          {process.env.BETA_BADGE && (
+          {process.env.NEXT_PUBLIC_BETA_BADGE && (
             <span className=' p-1 ml-2 text-xs text-yellow-200 bg-yellow-500 rounded'>
               BETA
             </span>
