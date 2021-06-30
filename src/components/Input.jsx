6c4fcb0eaf30cf0d590a16853b858817a42bbea9
Ema Lorenzo <aemalorenzo@gmail.com>
2021-06-30 14:47:16 -0300

    input component styles

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -17,7 +17,7 @@ export const Input = React.forwardRef(({ error, ...props }, ref) => (
       )}
     </AnimatePresence>
     <input
-      className='relative w-full max-w-md p-2 pl-5 text-sm font-medium border border-black rounded dark:border-gray-800 hover:border-gray-500 dark:hover:border-gray-600 focus:outline-none focus:ring-1 dark:focus:ring-yellow-500 focus:ring-black'
+      className='relative w-full max-w-md p-2 pl-5 text-sm font-medium bg-white border border-black rounded dark:bg-gray-800 dark:border-gray-800 hover:border-gray-500 dark:hover:border-gray-600 focus:outline-none focus:ring-1 dark:focus:ring-yellow-500 focus:ring-black'
       {...props}
     />
   </div>
