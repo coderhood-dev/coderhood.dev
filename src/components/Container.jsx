1e7047d6d92ca44672f7e38ba6e8a6f46c9ae61a
Ema Lorenzo <aemalorenzo@gmail.com>
2021-06-30 14:45:54 -0300

    header fixed

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,20 +3,16 @@ import { Header } from '@/components/Header'
 
 export const Container = ({ title, children, column, className }) => {
   return (
-    <>
-      <motion.div
-        className={`w-full h-full`}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { delay: 0.2 } }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <Header />
-        <main className={`h-full flex ${className}`}>{children}</main>
-        <footer className='px-20 text-lg text-gray-100 bg-gray-900'>
-          Footer
-        </footer>
-      </motion.div>
-    </>
+    <motion.div
+      className={`w-full h-full overflow-x-hidden`}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 0.2 } }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Header className='fixed top-0' />
+      <main className={`h-full pt-20 flex ${className}`}>{children}</main>
+      <footer className='px-20 text-lg text-gray-100 bg-gray-900'>Footer</footer>
+    </motion.div>
   )
 }
