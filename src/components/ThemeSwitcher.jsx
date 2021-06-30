ed900df2f22de777d213be3bcaabd4e3b082dec1
Ema Lorenzo <aemalorenzo@gmail.com>
2021-06-30 14:46:40 -0300

    removed r3f setup

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -2,104 +2,103 @@ import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import { motion, AnimatePresence } from 'framer-motion'
 
-import useStore from '@/lib/store'
-
 export const ThemeSwitcher = ({ className }) => {
   const [mounted, setMounted] = useState(false)
+
+  // nextTheme is a key to trigger motion animations
+  const [nextTheme, setNextTheme] = useState(undefined)
+
   const [{ scale, margin, zIndex }, setStyle] = useState({
     scale: 1,
     margin: '1.25rem',
-    zIndex: 1,
+    zIndex: 0,
   })
-  const { resolvedTheme, setTheme } = useTheme()
-
-  const theme = useStore((state) => state.theme)
-
-  useStore.setState({
-    theme: resolvedTheme === 'dark' ? 'light' : 'dark',
-  })
+  const data = useTheme()
+  const { resolvedTheme, setTheme } = data
 
   // After mounting, we have access to the theme
   useEffect(() => setMounted(true), [])
 
+  const handlePress = () => {
+    setStyle({ scale: 1.5, margin: '2rem', zIndex: 20 })
+    setTimeout(() => {
+      setStyle({ scale: 1, margin: '1.25rem', zIndex: 0 })
+    }, 800)
+
+    setNextTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+
+    setTimeout(() => {
+      setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+    }, 400)
+  }
+
   return (
-    <motion.button
-      aria-label='Toggle Dark Mode'
-      type='button'
-      className={`flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-200 rounded dark:bg-gray-800 focus:outline-none focus:ring-2 ring-yellow-500 ring-opacity-50 ${className}`}
-      onClick={() => {
-        setStyle({ scale: 2, margin: '3rem', zIndex: 20 })
-        setTimeout(() => {
-          setStyle({ scale: 1, margin: '1.25rem', zIndex: 1 })
-        }, 800)
-
-        useStore.setState({
-          algo: theme === 'dark' ? 'light' : 'dark',
-        })
-
-        setTimeout(() => {
-          setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-        }, 400)
-      }}
-      animate={{ scale, margin, zIndex }}
-    >
-      {mounted && (
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-          stroke='currentColor'
-          className='w-4 h-4 text-gray-800 dark:text-gray-200'
-        >
-          <AnimatePresence exitBeforeEnter>
-            {resolvedTheme === 'dark' ? (
-              <motion.path
-                key={resolvedTheme}
-                initial={{
-                  x: -20,
-                  opacity: 0.5,
-                  rotate: -45,
-                  pathLength: 0,
-                }}
-                animate={{
-                  x: 0,
-                  opacity: 1,
-                  rotate: 0,
-                  pathLength: 1,
-                }}
-                exit={{ x: 20, rotate: 45 }}
-                transition={{ duration: 0.3, ease: 'easeInOut' }}
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
-              />
-            ) : (
-              <motion.path
-                key={resolvedTheme}
-                initial={{
-                  x: -20,
-                  opacity: 0.5,
-                  rotate: -45,
-                  pathLength: 0,
-                }}
-                animate={{
-                  x: 0,
-                  opacity: 1,
-                  rotate: 0,
-                  pathLength: 1,
-                }}
-                exit={{ x: 20, rotate: 45 }}
-                transition={{ duration: 0.3, ease: 'easeInOut' }}
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
-              />
-            )}
-          </AnimatePresence>
-        </svg>
-      )}
-    </motion.button>
+    <div>
+      <motion.button
+        aria-label='Toggle Dark Mode'
+        type='button'
+        className={`right-0 top-0 absolute flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-200 rounded dark:bg-gray-800 focus:outline-none focus:ring-2 ring-yellow-500 ring-opacity-50 ${className}`}
+        onClick={handlePress}
+        animate={{ scale, margin, zIndex }}
+      >
+        {mounted && (
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+            stroke='currentColor'
+            className='w-4 h-4 text-gray-800 dark:text-gray-200'
+          >
+            <AnimatePresence exitBeforeEnter>
+              {resolvedTheme === 'dark' ? (
+                <motion.path
+                  key={resolvedTheme}
+                  initial={{
+                    x: -20,
+                    opacity: 0.5,
+                    rotate: -45,
+                    pathLength: 0,
+                  }}
+                  animate={{
+                    x: 0,
+                    opacity: 1,
+                    rotate: 0,
+                    pathLength: 1,
+                  }}
+                  exit={{ x: 20, rotate: 45 }}
+                  transition={{ duration: 0.3, ease: 'easeInOut' }}
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
+                />
+              ) : (
+                <motion.path
+                  key={resolvedTheme}
+                  initial={{
+                    x: -20,
+                    opacity: 0.5,
+                    rotate: -45,
+                    pathLength: 0,
+                  }}
+                  animate={{
+                    x: 0,
+                    opacity: 1,
+                    rotate: 0,
+                    pathLength: 1,
+                  }}
+                  exit={{ x: 20, rotate: 45 }}
+                  transition={{ duration: 0.3, ease: 'easeInOut' }}
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+                />
+              )}
+            </AnimatePresence>
+          </svg>
+        )}
+      </motion.button>
+    </div>
   )
 }
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,54 +1,19 @@
-import { Children } from 'react'
-import dynamic from 'next/dynamic'
 import { ThemeProvider } from 'next-themes'
 
-import Dom from '@/layouts/_dom'
+import { TransitionManager } from '@/components/TransitionManager'
 import { AuthProvider } from '@/context/Auth'
 
 import '@/styles/index.css'
 
-let LCanvas = null
-if (process.env.NODE_ENV === 'production') {
-  LCanvas = dynamic(() => import('@/layouts/_canvas'), {
-    ssr: false,
-  })
-} else {
-  LCanvas = require('@/layouts/_canvas').default
-}
-
-function Layout({ dom }) {
-  return <>{dom && <Dom>{dom}</Dom>}</>
-}
-
-const ForwardPropsToR3fComponent = ({ comp, pageProps }) => {
-  let r3fArr = []
-  let compArr = []
-
-  Children.forEach(comp(pageProps).props.children, (child) => {
-    if (child.props && child.props.r3f) {
-      r3fArr.push(child)
-    } else {
-      compArr.push(child)
-    }
-  })
-
-  return (
-    <>
-      {r3fArr && <LCanvas>{r3fArr}</LCanvas>}
-      {compArr && <Layout dom={compArr} />}
-    </>
-  )
-}
-
 function App({ Component, pageProps }) {
   return (
-    <>
-      <ThemeProvider attribute='class'>
-        <AuthProvider>
-          <ForwardPropsToR3fComponent comp={Component} pageProps={pageProps} />
-        </AuthProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider attribute='class'>
+      <AuthProvider>
+        <TransitionManager transitionKey={pageProps.title}>
+          <Component {...pageProps} />
+        </TransitionManager>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 
