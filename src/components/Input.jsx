16579bc8f8e8e72524f90c43c5bf09c99e1517b2
Ema Lorenzo <aemalorenzo@gmail.com>
2021-07-08 15:54:46 -0300

    some components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
-export const Input = React.forwardRef(({ error, ...props }, ref) => (
+export const Input = React.forwardRef(({ error, placeholder, ...props }, ref) => (
   <div className={`relative flex flex-col items-start w-full my-2 ${error && 'mb-8'}`}>
     <AnimatePresence exitBeforeEnter>
       {error && (
@@ -16,6 +16,7 @@ export const Input = React.forwardRef(({ error, ...props }, ref) => (
         </motion.span>
       )}
     </AnimatePresence>
+    <label className='mb-2 text-xs dark:text-gray-300'>{placeholder}</label>
     <input
       className='relative w-full max-w-md p-2 pl-5 text-sm font-medium bg-white border border-black rounded dark:bg-gray-800 dark:border-gray-800 hover:border-gray-500 dark:hover:border-gray-600 focus:outline-none focus:ring-1 dark:focus:ring-yellow-500 focus:ring-black'
       {...props}
